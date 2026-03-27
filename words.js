7c492d788c88011475303ecd51ba2efdb64c2c07
blossomedu <blossometeacher@gmail.com>
2026-03-28 08:16:11 +0900

    Update words.js

--- words.js
+++ words.js
@@ -11040,108 +11040,108 @@
 23-30|subsequent|(형) 차후의, 이후의|subsequent events|그 이후의 사건들
 23-30|consequent|(형) 결과로 일어나는|consequent loss|그 결과로 생긴 손실
 
-잠신중-1-1|a lot|많이|a lot|많이
-잠신중-1-1|always|(부) 항상, 언제나|always|(부) 항상, 언제나
-잠신중-1-1|active|(형) 활동적인|active|(형) 활동적인
-잠신중-1-1|character|(명) (책, 영화 등의) 등장인물|character|(명) (책, 영화 등의) 등장인물
-잠신중-1-1|clearly|(부) 분명히, 또렷하게|clearly|(부) 분명히, 또렷하게
-잠신중-1-1|club|(명) 동아리, 동호회|club|(명) 동아리, 동호회
-잠신중-1-1|common|(형) 공통의, 공동의|common|(형) 공통의, 공동의
-잠신중-1-1|conversation|(명) 대화|conversati|(명) 대화
-잠신중-1-1|cotton candy|(명) 솜사탕|cotton car|(명) 솜사탕
-잠신중-1-1|creative|(형) 창의적인|creative|(형) 창의적인
-잠신중-1-1|draw|(동) (그림을) 그리다|draw|(동) (그림을) 그리다
-잠신중-1-1|during|(전) ~동안, ~하는 중에|during|(전) ~동안, ~하는 중에
-잠신중-1-1|easily|(부) 쉽게|easily|(부) 쉽게
-잠신중-1-1|enjoy|(동) 즐기다|enjoy|(동) 즐기다
-잠신중-1-1|everything|(대) 모든 것|everything|(대) 모든 것
-잠신중-1-1|friendly|(형) 친절한|friendly|(형) 친절한
-잠신중-1-1|glass|(명) 유리|glass|(명) 유리
-잠신중-1-1|honest|(형) 정직한|honest|(형) 정직한
-잠신중-1-1|horror|(명) 공포|horror|(명) 공포
-잠신중-1-1|honeybee|(명) 꿀벌|honeybee|(명) 꿀벌
-잠신중-1-1|interesting|(형) 재미있는, 흥미로운|interesting|(형) 재미있는, 흥미로운
-잠신중-1-1|introduce|(동) 소개하다|introduce|(동) 소개하다
-잠신중-1-1|lie|(동) 거짓말하다 (명) 거짓말|lie|(동) 거짓말하다 (명) 거짓말
-잠신중-1-1|liar|(명) 거짓말쟁이|liar|(명) 거짓말쟁이
-잠신중-1-1|like|(동) 좋아하다 (전) ~처럼, ~같은|like|(동) 좋아하다 (전) ~처럼, ~같은
-잠신중-1-1|never|(부) 결코[절대] ~않다|never|(부) 결코[절대] ~않다
-잠신중-1-1|other|(대) 다른 사람[것]|other|(대) 다른 사람[것]
-잠신중-1-1|poster|(명) 포스터|poster|(명) 포스터
-잠신중-1-1|patient|(형) 참을성[인내심] 있는|patient|(형) 참을성[인내심] 있는
-잠신중-1-1|perfect|(형) 완벽한|perfect|(형) 완벽한
-잠신중-1-1|really|(부) 정말로, 아주|really|(부) 정말로, 아주
-잠신중-1-1|smart|(형) 똑똑한, 영리한|smart|(형) 똑똑한, 영리한
-잠신중-1-1|share|(동) 공유하다, 나누다|share|(동) 공유하다, 나누다
-잠신중-1-1|sweet|(형) 달콤한; 상냥한, (명) 단것|sweet|(형) 달콤한; 상냥한, (명) 단것
-잠신중-1-1|sometimes|(부) 때때로, 가끔|sometimes|(부) 때때로, 가끔
-잠신중-1-1|then|(부) 그러면; 그 다음에|then|(부) 그러면; 그 다음에
-잠신중-1-1|thought|(명) 생각, 사고|thought|(명) 생각, 사고
-잠신중-1-1|together|(부) 함께, 같이|together|(부) 함께, 같이
-잠신중-1-1|turtle|(명) 바다거북|turtle|(명) 바다거북
-잠신중-1-1|worry|(동) 걱정하다 (명) 걱정(거리)|worry|(동) 걱정하다 (명) 걱정(거리)
-잠신중-1-1|do one's best|최선을 다하다|do one's b|최선을 다하다
-잠신중-1-1|in harmony with|~와 조화[협조]하여|in harmon|~와 조화[협조]하여
-잠신중-1-1|characterize|(동) ~의 특징이 되다, 특징짓다|characteri|(동) ~의 특징이 되다, 특징짓다
-잠신중-1-1|energetic|(형) 활기 있는, 활동적인|energetic|(형) 활기 있는, 활동적인
-잠신중-1-1|participation|(명) 참여, 참가|participati|(명) 참여, 참가
-잠신중-1-1|imagination|(명) 상상|imaginatic|(명) 상상
-잠신중-1-1|difficulty|(명) 어려움, 고난|difficulty|(명) 어려움, 고난
-잠신중-1-1|effort|(명) 노력|effort|(명) 노력
-잠신중-1-1|pleasure|(명) 기쁨|pleasure|(명) 기쁨
-잠신중-1-1|agreement|(명) 동의|agreemen|(명) 동의
+25-1	a lot	많이	a lot
+25-1	always	(부) 항상, 언제나	always
+25-1	active	(형) 활동적인	active
+25-1	character	(명) (책, 영화 등의) 등장인물	character
+25-1	clearly	(부) 분명히, 또렷하게	clearly
+25-1	club	(명) 동아리, 동호회	club
+25-1	common	(형) 공통의, 공동의 	common
+25-1	conversation	(명) 대화	conversation
+25-1	cotton candy	(명) 솜사탕	cotton candy
+25-1	creative	(형) 창의적인 	creative
+25-1	draw	(동) (그림을) 그리다	draw
+25-1	during	(전) ~동안, ~하는 중에	during
+25-1	easily	(부) 쉽게	easily
+25-1	enjoy	(동) 즐기다	enjoy
+25-1	everything	(대) 모든 것	everything
+25-1	friendly	(형) 친절한	friendly
+25-1	glass	(명) 유리	glass
+25-1	honest	(형) 정직한	honest
+25-1	horror	(명) 공포	horror
+25-1	honeybee	(명) 꿀벌	honeybee
+25-1	interesting	(형) 재미있는, 흥미로운	interesting
+25-1	introduce	(동) 소개하다	introduce
+25-1	lie	(동) 거짓말하다 (명) 거짓말	lie
+25-1	liar	(명) 거짓말쟁이	liar
+25-1	like	(동) 좋아하다 (전) ~처럼, ~같은	like
+25-1	never	(부) 결코[절대] ~않다	never
+25-1	other	(대) 다른 사람[것]	other
+25-1	poster	(명) 포스터	poster
+25-1	patient	(형) 참을성[인내심] 있는	patient
+25-1	perfect	(형) 완벽한	perfect
+25-1	really	(부) 정말로, 아주	really
+25-1	smart	(형) 똑똑한, 영리한	smart
+25-1	share	(동) 공유하다, 나누다	share
+25-1	sweet	(형) 달콤한; 상냥한, (명) 단것	sweet
+25-1	sometimes	(부) 때때로, 가끔	sometimes
+25-1	then	(부) 그러면; 그 다음에	then
+25-1	thought	(명) 생각, 사고	thought
+25-1	together	(부) 함께, 같이	together
+25-1	turtle	(명) 바다거북	turtle
+25-1	worry	(동) 걱정하다 (명) 걱정(거리)	worry
+25-1	do one's best	최선을 다하다	do one's best
+25-1	in harmony with	~와 조화[협조]하여	in harmony with
+25-1	characterize	(동) ~의 특징이 되다, 특징짓다	characterize
+25-1	energetic	(형) 활기 있는, 활동적인	energetic
+25-1	participation	(명) 참여, 참가	participation
+25-1	imagination	(명) 상상	imagination
+25-1	difficulty	(명) 어려움, 고난	difficulty
+25-1	effort	(명) 노력	effort
+25-1	pleasure	(명) 기쁨	pleasure
+25-1	agreement	(명) 동의	agreement
 
+26-1	beetle	(명) 딱정벌레	beetle	(명) 딱정벌레
+26-1	behavior	(명) 행동	behavior	(명) 행동
+26-1	blog	(명) 블로그	blog	(명) 블로그
+26-1	congress	(명) 의회, 국회	congress	(명) 의회, 국회
+26-1	craft	(동) 공예품을 만들다	craft	(동) 공예품을 만들다
+26-1	feed	(동) 먹이를 주다	feed	(동) 먹이를 주다
+26-1	gift	(명) 선물	gift	(명) 선물
+26-1	grab	(동) 붙잡다	grab	(동) 붙잡다
+26-1	highlight	(명) 하이라이트	highlight	(명) 하이라이트
+26-1	important	(형) 중요한	important	(형) 중요한
+26-1	insect	(명) 곤충	insect	(명) 곤충
+26-1	journey	(명) 여정	journey	(명) 여정
+26-1	maybe	(부) 아마도, 어쩌면	maybe	(부) 아마도, 어쩌면
+26-1	once	(부) 한 번	once	(부) 한 번
+26-1	photo	(명) 사진	photo	(명) 사진
+26-1	piece	(명) 조각	piece	(명) 조각
+26-1	reach	(동) ~에 도착하다	reach	(동) ~에 도착하다
+26-1	relax	(동) 편하게 하다	relax	(동) 편하게 하다
+26-1	sense	(명) 감각, 느낌	sense	(명) 감각, 느낌
+26-1	spend	(동) (시간/돈을) 보내다, 쓰다	spend	(동) (시간/돈을) 보내다, 쓰다
+26-1	stamp	(명) 우표	stamp	(명) 우표
+26-1	thousand	(명) 천, 1,000	thousand	(명) 천, 1,000
+26-1	tiny	(형) 아주 작은	tiny	(형) 아주 작은
+26-1	treasure	(명) 보물	treasure	(명) 보물
+26-1	yoga	(명) 요가	yoga	(명) 요가
+26-1	hard	(형) 단단한	hard	(형) 단단한
+26-1	regularly	(부) 규칙적으로	regularly	(부) 규칙적으로
+26-1	experience	(명) 경험	experience	(명) 경험
+26-1	especially	(부) 특히	especially	(부) 특히
+26-1	suddenly	(부) 갑자기	suddenly	(부) 갑자기
+26-1	firmly	(부) 단단하게	firmly	(부) 단단하게
+26-1	value	(명) 가치	value	(명) 가치
+26-1	effect	(명) 영향	effect	(명) 영향
+26-1	distance	(명) 거리	distance	(명) 거리
+26-1	occasion	(명) 상황, 경우	occasion	(명) 상황, 경우
+26-1	amount	(명) 총 합계	amount	(명) 총 합계
+26-1	be separated	분리되다	be separated	분리되다
+26-1	be broken off	떨어져 나오다	be broken off	떨어져 나오다
+26-1	a whole	전체	a whole	전체
+26-1	certain	(형) 특정한	certain	(형) 특정한
+26-1	point	(명) 부분, 지점	point	(명) 부분, 지점
+26-1	tense	(형) 긴장감 있는, 팽팽한	tense	(형) 긴장감 있는, 팽팽한
+26-1	stick on	~에 붙이다, 부착하다	stick on	~에 붙이다, 부착하다
+26-1	envelop	(명) 봉투	envelop	(명) 봉투
+26-1	mail	(동) 메일을 보내다	mail	(동) 메일을 보내다
+26-1	valuable	(형) 가치 있는	valuable	(형) 가치 있는
+26-1	object	(명) 물체	object	(명) 물체
+26-1	precious	(형) 귀중한	precious	(형) 귀중한
+26-1	mental	(형) 정신의	mental	(형) 정신의
+26-1	physical	(형) 신체의	physical	(형) 신체의
 
-잠신중-2-1|beetle|(명) 딱정벌레|beetle|(명) 딱정벌레
-잠신중-2-1|behavior|(명) 행동|behavior|(명) 행동
-잠신중-2-1|blog|(명) 블로그|blog|(명) 블로그
-잠신중-2-1|congress|(명) 의회, 국회|congress|(명) 의회, 국회
-잠신중-2-1|craft|(동) 공예품을 만들다|craft|(동) 공예품을 만들다
-잠신중-2-1|feed|(동) 먹이를 주다|feed|(동) 먹이를 주다
-잠신중-2-1|gift|(명) 선물|gift|(명) 선물
-잠신중-2-1|grab|(동) 붙잡다|grab|(동) 붙잡다
-잠신중-2-1|highlight|(명) 하이라이트|highlight|(명) 하이라이트
-잠신중-2-1|important|(형) 중요한|important|(형) 중요한
-잠신중-2-1|insect|(명) 곤충|insect|(명) 곤충
-잠신중-2-1|journey|(명) 여정|journey|(명) 여정
-잠신중-2-1|maybe|(부) 아마도, 어쩌면|maybe|(부) 아마도, 어쩌면
-잠신중-2-1|once|(부) 한 번|once|(부) 한 번
-잠신중-2-1|photo|(명) 사진|photo|(명) 사진
-잠신중-2-1|piece|(명) 조각|piece|(명) 조각
-잠신중-2-1|reach|(동) ~에 도착하다|reach|(동) ~에 도착하다
-잠신중-2-1|relax|(동) 편하게 하다|relax|(동) 편하게 하다
-잠신중-2-1|sense|(명) 감각, 느낌|sense|(명) 감각, 느낌
-잠신중-2-1|spend|(동) (시간/돈을) 보내다, 쓰다|spend|(동) (시간/돈을) 보내다, 쓰다
-잠신중-2-1|stamp|(명) 우표|stamp|(명) 우표
-잠신중-2-1|thousand|(명) 천, 1,000|thousand|(명) 천, 1,000
-잠신중-2-1|tiny|(형) 아주 작은|tiny|(형) 아주 작은
-잠신중-2-1|treasure|(명) 보물|treasure|(명) 보물
-잠신중-2-1|yoga|(명) 요가|yoga|(명) 요가
-잠신중-2-1|hard|(형) 단단한|hard|(형) 단단한
-잠신중-2-1|regularly|(부) 규칙적으로|regularly|(부) 규칙적으로
-잠신중-2-1|experience|(명) 경험|experience|(명) 경험
-잠신중-2-1|especially|(부) 특히|especially|(부) 특히
-잠신중-2-1|suddenly|(부) 갑자기|suddenly|(부) 갑자기
-잠신중-2-1|firmly|(부) 단단하게|firmly|(부) 단단하게
-잠신중-2-1|value|(명) 가치|value|(명) 가치
-잠신중-2-1|effect|(명) 영향|effect|(명) 영향
-잠신중-2-1|distance|(명) 거리|distance|(명) 거리
-잠신중-2-1|occasion|(명) 상황, 경우|occasion|(명) 상황, 경우
-잠신중-2-1|amount|(명) 총 합계|amount|(명) 총 합계
-잠신중-2-1|be separated|분리되다|be separat|분리되다
-잠신중-2-1|be broken off|떨어져 나오다|be broken|떨어져 나오다
-잠신중-2-1|a whole|전체|a whole|전체
-잠신중-2-1|certain|(형) 특정한|certain|(형) 특정한
-잠신중-2-1|point|(명) 부분, 지점|point|(명) 부분, 지점
-잠신중-2-1|tense|(형) 긴장감 있는, 팽팽한|tense|(형) 긴장감 있는, 팽팽한
-잠신중-2-1|stick on|~에 붙이다, 부착하다|stick on|~에 붙이다, 부착하다
-잠신중-2-1|envelop|(명) 봉투|envelop|(명) 봉투
-잠신중-2-1|mail|(동) 메일을 보내다|mail|(동) 메일을 보내다
-잠신중-2-1|valuable|(형) 가치 있는|valuable|(형) 가치 있는
-잠신중-2-1|object|(명) 물체|object|(명) 물체
-잠신중-2-1|precious|(형) 귀중한|precious|(형) 귀중한
-잠신중-2-1|mental|(형) 정신의|mental|(형) 정신의
-잠신중-2-1|physical|(형) 신체의|physical|(형) 신체의
 
   `;
   // 👆 여기까지가 네가 앞으로 수정·추가할 영역
